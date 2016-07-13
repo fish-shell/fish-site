cb8e7cb15c1ddebb97d04e2d11996965f69f2b99
Aaron Gyes <me@aaron.gy>
2016-07-13 14:28:20 -0700

    Make release notes script work on python 2 and 3.
    General spruce-up
    Make python2.6 or above to be more like 3 where possible.
    (This type of treatment should be given to all our python scripts.)

--- tools/htmlize_relnotes.py
+++ tools/htmlize_relnotes.py
@@ -1,20 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
+import threading
 import sys
 import re
 import cgi
-import urllib2
-import threading
+import io
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlopen
 
 def determine_url(numstr):
     """ Accesses a github issue URL, and returns the forwarded URL
         This allows us to distinguish pull requests from issues.
     """
     probe_url = 'https://github.com/fish-shell/fish-shell/issues/' + numstr
-    req = urllib2.urlopen(probe_url)
+    req = urlopen(probe_url)
     res = req.geturl()
     return res
-        
 
 def annotate_items(contents):
     """ Looks for text of the form (#123), and annotates it with the URL to the
@@ -23,18 +30,20 @@ def annotate_items(contents):
     # Determine item numbers
     pattern = r'\(#(\d+)\)'
     matches = re.finditer(pattern, contents)
-    
     # Resolve numstrs to URLs
     numstr_to_url = {}
+
     def resolve_numstr(numstr):
         numstr_to_url[numstr] = determine_url(numstr)
     threads = []
     for match in matches:
         numstr = match.group(1)
         threads.append(threading.Thread(target=resolve_numstr, args=(numstr,)))
-    for t in threads: t.start()
-    for t in threads: t.join()
-    
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
     # Perform annotation
     def annotate_1(match):
         numstr = match.group(1)
@@ -42,31 +51,31 @@ def annotate_items(contents):
             return match.group(0)
         url = numstr_to_url[numstr]
         return '(<a href="%s">#%s</a>)' % (url, match.group(1))
-        
+
     return re.sub(pattern, annotate_1, contents)
-    
+
 def backticks_to_code(contents):
     """ Replaces `foo` with <code>foo</code> """
     result_lines = []
     for line in contents.split('\n'):
         comps = line.split('`')
         # <code> goes around every odd item
-        for idx in xrange(len(comps)):
+        for idx in range(len(comps)):
             if idx % 2 == 1:
                 comps[idx] = '<code>%s</code>' % comps[idx]
         result_lines.append(''.join(comps))
     return '\n'.join(result_lines)
 
-class Markdown2HTML:
+class Markdown2HTML(object):
     """ Extraordinarily stupid markdown to HTML converter. This only handles ## and - """
     def __init__(self):
         self.in_list = False
         self.result_lines = []
-    
+
     def emit_surround(self, line, tag):
         indent = ' '*4 if self.in_list else ''
         self.result_lines.append('%s<%s>%s</%s>' % (indent, tag, cgi.escape(line.strip()), tag))
-        
+
     def emit_open_close_list(self, is_open):
         if is_open and not self.in_list:
             self.in_list = True
@@ -74,10 +83,9 @@ class Markdown2HTML:
         elif not is_open and self.in_list:
             self.in_list = False
             self.result_lines.append('</ul>')
-    
+
     def emit_line(self, line):
         self.result_lines.append(cgi.escape(line))
-
 
 def markdown_2_html(text):
     cv = Markdown2HTML()
@@ -90,27 +98,27 @@ def markdown_2_html(text):
         else:
             cv.emit_line(line)
     cv.emit_open_close_list(False)
-    return '\n'.join(cv.result_lines)       
+    return '\n'.join(cv.result_lines)
 
 def htmlize(src_path):
-    with open(src_path, 'r') as fd:
+    with io.open(src_path, 'r') as fd:
         contents = fd.read()
-        
+
     contents = markdown_2_html(contents)
     contents = annotate_items(contents)
     contents = backticks_to_code(contents)
-    print contents
+    print(contents)
 
 def usage():
-    print """\
-Usage: htmlize_relnotes.py.py file...
+    print("""\
+Usage: htmlize_relnotes.py.py file…
 
 htmlize_relnotes.py builds HTML relnotes from GitHub relnotes.
-"""
+""")
 
 if __name__ == "__main__":
     paths = sys.argv[1:]
-    if not paths:
+    if not paths or "--help" in paths:
         usage()
         sys.exit(-1)
     for path in paths:
