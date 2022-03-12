db0462b0863c5155d641e18b4e78a01aba5fa0c6
Fabian Homborg <FHomborg@gmail.com>
2022-03-12 16:40:58 +0100

    Also re-render docs

--- site/_site/docs/current/_static/documentation_options.js
+++ site/_site/docs/current/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.3.1',
-    LANGUAGE: 'None',
+    VERSION: '3.4.0',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
